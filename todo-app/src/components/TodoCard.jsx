4253d7e1a6e73e923bf76734070c8a8c5a90fdea
Ku0t <kuotthiik@gmail.com>
2025-10-04 23:36:50 +1000

    Finished First App

--- todo-app/src/components/TodoCard.jsx
+++ todo-app/src/components/TodoCard.jsx
@@ -1,15 +1,18 @@
 export function TodoCard(props) {
-    const { todoIndex, todos } = props
-    const todo = todos[todoIndex]
-    console.log(todo)
+    const { todo, handleDeleteTodo, todoIndex, handleCompleteTodo } = props
+
     return (
         <div className="card todo-item">
             <p>{todo.input}</p>
             <div className="todo-buttons">
-                <button disabled={todo.complete}>
+                <button onClick={() => {
+                    handleCompleteTodo(todoIndex)
+                }} disabled={todo.complete}>
                     <h6>Done</h6>
                 </button>
-                <button>
+                <button onClick={() => {
+                    handleDeleteTodo(todoIndex)
+                }}>
                     <h6>Delete</h6>
                 </button>
             </div>
